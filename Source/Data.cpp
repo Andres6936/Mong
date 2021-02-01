6601fce7997b10dc56909275cf456f594957af62
Joan Andrés <andres6936@live.com>
2021-02-01 10:19:00 -0500

    Short cut to uncompressed data

--- Source/Data.cpp
+++ Source/Data.cpp
@@ -1,10 +1,11 @@
 // Joan Andrés (@Andres6936) Github.
 
 #include "Mong/Data.hpp"
+#include "Cup/Properties/Readable.h"
 
 using namespace Mong;
 
 Data::Data(const Chunk& _chunk, const Information& _info) : IDAT(_chunk), IHDR(_info)
 {
-
+	std::vector<std::byte> uncompressed = Cup::Properties::Readable::readBinary("IDAT.uncompress");
 }
